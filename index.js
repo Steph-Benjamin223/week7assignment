bdadbc09be688c5009d8307ce877138a1a5fb81c
Steph-Benjamin223 <stephanie.benjamin@gmail.com>
2023-11-17 15:48:34 -0600

    finishthruprob7

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-//1.
 // 1. Create an array called ages
 let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
-// 2. Subtract the first element from the last element programmatically
+//  Subtract the first element from the last element programmatically
 let difference = ages[ages.length - 1] - ages[0];
 console.log(
   `The difference between the first and last elements is ${difference}.`
 );
 
-// 3. Add a new age to the array and repeat the subtraction step to ensure it's dynamic
+// Add a new age to the array and repeat the subtraction step to ensure it's dynamic
 ages.push(50);
 difference = ages[ages.length - 1] - ages[0];
 console.log(
   `The difference between the first and last elements is now ${difference}.`
 );
 
-// 4. Use a loop to calculate the average age
+//Use a loop to calculate the average age
 let totalAge = 0;
 
 for (let i = 0; i < ages.length; i++) {
@@ -30,7 +29,7 @@ console.log(`The average age is ${averageAge}.`);
 //  Create an array called names
 let names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
 
-//  Use a loop to calculate the average number of letters per name
+//Use a loop to calculate the average number of letters per name
 let totalLetters = 0;
 
 for (let i = 0; i < names.length; i++) {
@@ -50,8 +49,20 @@ for (let i = 0; i < names.length; i++) {
 
 console.log(`The concatenated names are: ${concatenatedNames}`);
 
+//3. How do you access the last element of any array?
+// you can use the index array.length -1.  An example is below:
+let array = [1, 2, 3, 4, 5];
+let lastElement = array[array.length - 1];
+console.log(lastElement); // output: 5
+
+//4. How do you access the first element of any array?
+// you can use the index 0.  An example is below:
+
+let firstElement = array[0];
+console.log(firstElement); // output: 1
+
 // 5. Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
-// For example:
+
 let nameLengths = [];
 for (let i = 0; i < names.length; i++) {
   const length = names[i].length;
@@ -66,3 +77,18 @@ let sum = 0;
 for (let i = 0; i < nameLengths.length; i++) {
   sum += nameLengths[i].length;
 }
+
+//7. Write a function that takes two parameters, word and n,
+//as arguments and returns the word concatenated to itself n number of times.
+//(i.e. if I pass in ‘Hello’ and 3, I would expect the function to return
+//‘HelloHelloHello’).
+
+function concatenateWord(word, n) {
+  let concatenatedWord = "";
+  for (let i = 0; i < n; i++) {
+    concatenatedWord += word;
+  }
+  console.log(concatenatedWord);
+}
+
+concatenateWord("Hello", 6);
